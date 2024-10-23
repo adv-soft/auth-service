07b484cec2b22c5584abec279b73106f82750b4f
Timothy Axel Winarto <timothy@roosh.online>
2024-10-23 10:46:36 +0200

    test build

--- obj/Debug/net8.0/AuthService.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50c6e25528d091c2f56d12be090408fd7da5c62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33ba048c28985c3428adc0e622b50bcdb9d7b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
